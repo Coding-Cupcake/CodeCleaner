952c86248274ce7b610a2da1a7d7126b7b0c02d0
Patrick Weiß <patrick.weiss2206@gmail.com>
2015-12-31 10:21:31 +0100

    Unique file names, default editor opening

--- src/Cleaner.py
+++ src/Cleaner.py
@@ -1,18 +1,21 @@
 
 import os
-from Utils import createTempFile, YES, NO, QUIT
+from Utils import createSourceFile, createResultFile, YES, NO, QUIT
 
 'Create temporary source code storage file'
-os.system("start "+createTempFile())
+FilenameSource = createSourceFile()
+FilenameResult= createResultFile()
+os.system("start "+FilenameSource)
 
-print("Please copy your code into the temporary sourceCode file")
+'Instruct the user to copy the code in there and close the file'
+print("Please copy your code into the temporary sourceCode file and save/close it")
 
 while 1:
     ready = raw_input("\n" + "Have you saved and closed the file? (y/n)")
     if ready.upper() == YES:
         print("Processing file ...")
-        sourceCode = open("sourceCode.txt", "r")
-        resultCode = open("resultCode.txt", "w")
+        sourceCode = open(FilenameSource, "r")
+        resultCode = open(FilenameResult, "w")
         for line in sourceCode:
             i = 0
             while line[i].isdigit():
@@ -26,6 +29,7 @@ while 1:
     
     else:
         if ready.upper() == QUIT:
+            os.remove(FilenameSource)
             exit()
         print(ready + " is not a valid input. Type in y or n to continue, q to quit.")
         
@@ -33,10 +37,10 @@ while 1:
 sourceCode.close()
 resultCode.close()
 
-cleanup = raw_input("\n" + "Do you want to remove the temporary files? (y/n)")
+cleanup = raw_input("\n" + "Do you want to remove the created files? (y/n)")
 
 if cleanup.upper() == "Y":
-    os.remove("sourceCode.txt")
-    os.remove("resultCode.txt")
+    os.remove(FilenameSource)
+    os.remove(FilenameResult)
 
     
--- src/Utils.py
+++ src/Utils.py
@@ -1,9 +1,20 @@
 
+from datetime import datetime
+
 YES = "Y"
 NO  = "N"
 QUIT = "Q"
+SOURCE = "sourceCode"
+RESULT = "resultCode"
 
-def createTempFile():
-    sourceCode = open("sourceCode.txt", "w")
+'Create a file storing the copied source code'
+def createSourceFile():
+    FILENAME = SOURCE + datetime.now().strftime('%Y-%m-%d-%I-%M-%S') + ".txt"
+    sourceCode = open(FILENAME, "w")
     sourceCode.close()
-    return "sourceCode.txt"
+    return FILENAME
+
+'Create the name of the file storing the formatted result'
+def createResultFile():
+    FILENAME = RESULT + datetime.now().strftime('%Y-%m-%d-%I-%M-%S') + ".txt"
+    return FILENAME
